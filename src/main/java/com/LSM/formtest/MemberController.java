3a4eaf2fe645627edee0a8f4a22506c044d73bc0
LSM-1020 <smmlee@naver.com>
2025-08-27 10:41:53 +0900

    model로 Ok페이지 데이터 전달

---
+++ src/main/java/com/LSM/formtest/MemberController.java
@@ -0,0 +1,35 @@
+package com.LSM.formtest;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+
+	@RequestMapping(value ="/login") 
+		public String login(HttpServletRequest request, Model model) {
+		return "login";
+	}
+	@RequestMapping(value ="/loginOk") 
+	public String loginOk(HttpServletRequest request, Model model, HttpSession session, HttpServletResponse response) {
+		String mid = request.getParameter("mid"); //login.jsp에서 클라이언트가 입력한 값 불러오기 그리고 선언
+		String mpw = request.getParameter("mpw");
+		model.addAttribute("mid",mid); //model로 String mid를 실어서 loginOk로 보내줌 = request.setAttribute
+		model.addAttribute("mpw",mpw);
+		
+		if(mid.equals("tiger") && mpw.equals("12345")) {
+			session.setAttribute("sessionId", mid);//세션에 로그인한 아이디 올려두기
+			return "loginOk";
+		}else {
+			return "loginFail";
+		}
+		
+	
+}
+}
